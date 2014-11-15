e712b4130f05a40179e60aaaf29f5a1d3d7c5e8b
Michael Hartl <michael@michaelhartl.com>
2014-11-14 19:45:25 -0800

    Add through inject/reduce

--- lib/rubyconf.rb
+++ lib/rubyconf.rb
@@ -6,8 +6,8 @@ class Rubyconf
   def return_first_n_elements(enum, n)
     i = 0
     retval = []
-    enum.each do |e|
-      retval << e
+    enum.each do |k|
+      retval << k
       i += 1
       break if i >= n
     end
@@ -17,37 +17,89 @@ class Rubyconf
   end
 
   def any_are_odd(enum)
-    enum.each do |e|
-      return true if e.odd?
+    enum.each do |n|
+      return true if n.odd?
     end
     false
-    enum.any? { |e| e.odd? }
+    # enum.any? { |e| e.odd? }
   end
 
   def all_are_odd(enum)
     retval = true
-    enum.each do |e|
-      retval = false unless e.odd?
+    enum.each do |n|
+      retval = false unless n.odd?
     end
     retval
-    enum.all? { |e| e.odd? }
+    # enum.all? { |e| e.odd? }
   end
 
   def none_are_odd(enum)
     retval = true
-    enum.each do |e|
-      retval = false if e.odd?
+    enum.each do |n|
+      retval = false if n.odd?
     end
     retval
-    enum.none? { |e| e.odd? }
+    # enum.none? { |e| e.odd? }
   end
 
   def square_each_element(enum)
     squares = []
-    enum.each do |e|
-      squares << e**2
+    enum.each do |n|
+      squares << n**2
     end
     squares
-    enum.map { |e| e**2 }
+    # enum.map { |e| e**2 }
+    # enum.collect { |e| e**2 }
+  end
+
+  def find_evens(enum)
+    evens = []
+    enum.each do |n|
+      evens << n if n.even?
+    end
+    evens
+    # enum.find { |n| n.even?}
+    # enum.select { |n| n.even?}
+  end
+
+  def ignore_evens(enum)
+    non_evens = []
+    enum.each do |n|
+      non_evens << n unless n.even?
+    end
+    non_evens
+    # enum.reject { |n| n.even?}
+  end
+
+  def find_first_even(enum)
+    enum.each do |n|
+      return n if n.even?
+    end
+    # enum.find { |n| n.even? }
+    # enum.detect { |n| n.even? }
+  end
+
+  def make_pairs_of_numbers_and_squares(enum)
+    square_pairs = []
+    enum.each do |n|
+      square_pairs << [n, n**2]
+    end
+    square_pairs
+    enum.inject([]) do |square_pairs, n|
+      square_pairs << [n, n**2]
+    end
+  end
+
+  def product(enum)
+    factorial = 1
+    enum.each do |n|
+      factorial *= n
+    end
+    factorial
+    enum.inject(1) do |factorial, n|
+      factorial *= n
+    end
+    enum.inject(1, :*)
+    enum.reduce(1, :*)
   end
 end
--- test/rubyconf_test.rb
+++ test/rubyconf_test.rb
@@ -31,5 +31,26 @@ class TestRubyconf < Minitest::Test
     assert !@rubyconf.none_are_odd(@enum)
     assert  @rubyconf.none_are_odd([2, 4, 6])
   end
+
+  def test_find_evens
+    assert_equal [2, 4], @rubyconf.find_evens(@enum)
+  end
+
+  def test_ignore_evens
+    assert_equal [1, 3, 5], @rubyconf.ignore_evens(@enum)
+  end
+
+  def test_find_first_even
+    assert_equal 2, @rubyconf.find_first_even(@enum)
+  end
+
+  def test_make_pairs_of_numbers_and_squares
+    assert_equal [[1, 1], [2, 4], [3, 9], [4, 16], [5, 25]],
+                 @rubyconf.make_pairs_of_numbers_and_squares(@enum)
+  end
+
+  def test_product
+    assert_equal 120, @rubyconf.product(@enum)
+  end
 end
 
